aa3f41a3c590d86446cf13c9806f92bdfb032dcb
wangdong <wangdong7871@100.me>
2020-06-26 17:37:24 +0800

    use compressing

--- app/routes/index/index.js
+++ app/routes/index/index.js
@@ -4,7 +4,12 @@ const { handleSuccess, handleFail } = require('../../utils');
 const { query } = require('../../db');
 const koaBody = require("koa-body");
 const fs = require('fs');
-
+const compressing = require('compressing');
+const path = require('path');
+// const extract = require('extract-zip')
+// const decompress = require('decompress')
+// const zlib = require('zlib')
+// const zlib = require('zlib')
 let Router = koaRouter();
 
 Router.get('/get', async (ctx) => {
@@ -31,10 +36,47 @@ Router.get('/getTestJson', async (ctx) => {
   }
 });
 Router.post('/upload', koaBody(), (ctx) => {
+  if (ctx.request.files.file) {
 
-  console.log('ctx.request.files :>> ', ctx.request.files.file);
-  ctx.body = handleSuccess({
-    data: JSON.stringify(ctx.request.body),
-  });
+    // @todo 根据文件名创建对应的目录，并给出回调事件
+    // @todo 通过compressing.zip.uncompress(reader, filePath)方法将文件写入
+    // @todo 找到对应的index.html文件，返回路径
+
+
+    const reader = fs.createReadStream(ctx.request.files['file']['path']);
+    let filePath = `${path.resolve(__dirname, '../../www/assets')}/ddmccdn/`;
+    // let sourcePath = `${ctx.request.files['file']['path']}`
+    let sourcePath = `${__dirname}/PRD.zip`
+    console.log('filePath :>> ', filePath);
+    console.log('sourcePath :>> ', sourcePath);
+    compressing.zip.uncompress(reader, filePath)
+      .then(((res) => {
+        console.log(res);
+        ctx.body = handleSuccess({
+          data: ctx.request.files['file']['name'],
+        });
+      }))
+      .catch((error) => {
+        console.error(error);
+        ctx.body = handleSuccess({
+          data: '上传失败',
+        });
+      })
+
+
+
+
+
+
+
+
+
+
+
+  } else {
+    ctx.body = handleSuccess({
+      data: '上传失败',
+    });
+  }
 });
 module.exports = Router;
